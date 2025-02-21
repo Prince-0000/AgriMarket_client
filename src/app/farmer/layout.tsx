24649bb301aaaa263560dca81d244be2197ae8c9
Prince <princemehta516@gmail.com>
2025-02-22 00:55:24 +0530

    setup routes

---
+++ src/app/farmer/layout.tsx
@@ -0,0 +1,9 @@
+import FarmerPage from "./page";
+export default function FarmerLayout() {
+    return (
+     <div>
+        Farmer Layout
+      <FarmerPage />
+     </div>
+    );
+  }
---
+++ src/app/farmer/page.tsx
@@ -0,0 +1,7 @@
+export default function FarmerPage() {
+    return (
+     <div className='w-full h-screen'>
+        Farmer Page
+     </div>
+    );
+  }
