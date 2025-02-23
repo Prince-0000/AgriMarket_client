5352c22d9a92fb1dfc57b1944704130a68ef2046
Prince <princemehta516@gmail.com>
2025-02-24 01:54:16 +0530

    integerate backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+"use client"
 import {Button} from '@heroui/button'; 
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
 export default function Home() {
+  const [data, setData] = useState<string>("");
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      axios.get("http://localhost:4000/api/message")
+      .then((res)=>{
+        setData(String(res.data.message));
+        console.log(res);
+      })
+      .catch((err)=>{
+        console.error("Not found :", err);
+      })
+    
+    }
+    fetchData();
+  },[]);
   return (
    <div className='h-screen'>
       <Button>Click me</Button>
+      {data ? (
+        <div>
+          <p>{data}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
    </div>
   );
 }
